ee8847478d95b874279a4f992c8a67b0d7316212
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-07-30 14:52:47 -0500

    working version

---
+++ app/data/friends.js
@@ -0,0 +1,148 @@
+var friendData = [
+    {
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+       {
+    "name": "Billy",
+    "photo": "https://vignette.wikia.nocookie.net/grimadventures/images/6/61/Billy.gif/revision/latest?cb=20080723122111",
+    "scores": [
+    "4",
+    "3",
+    "3",
+    "5",
+    "4",
+    "1",
+    "2",
+    "4",
+    "2",
+    "2"
+    ]
+    },
+    {
+    "name": "Jacob",
+    "photo": "https://gph.is/2hyChXx",
+    "scores": [
+    "2",
+    "3",
+    "4",
+    "3",
+    "4",
+    "3",
+    "5",
+    "4",
+    "3",
+    "4"
+    ]
+    },
+    {
+    "name": "lokibeat",
+    "photo": "https://media.licdn.com/dms/image/C5603AQGR15Q72d2hOQ/profile-displayphoto-shrink_200_200/0?e=1538611200&v=beta&t=uwBI4cKzzgM1lS1mEFUBL7JxNYIhrbk6l2VmAiRN8Aw",
+    "scores": [
+    "2",
+    "2",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "4"
+    ]
+    }
+    ];
+
+    module.exports = friendData;
